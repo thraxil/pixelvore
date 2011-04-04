105dac26fe3177e3825dea46f11ce7e3d8645377
Anders Pearson <anders@columbia.edu>
2011-04-04 11:56:51 -0400

    got tahoe upload working!

--- main/tasks.py
+++ main/tasks.py
@@ -7,25 +7,33 @@ from poster.encode import multipart_encode, MultipartParam
 from poster.streaminghttp import register_openers
 import cStringIO
 
+
 @task(ignore_result=True)
 def ingest_image(slug,url):
     print "ingesting %s" % url
     filename = url.split("/")[-1]
     print filename
     imgdata = GET(url)
-    dfile = cStringIO.StringIO()
-    dfile.write(imgdata)
-    dfile.seek(0)
 
+
+
+
+    tmpfilename = "/tmp/%s" % filename
+    f = open(tmpfilename, "wb")
+    f.write(imgdata)
+    f.close()
+
+
+    source_file = open(tmpfilename,"rb")
     register_openers()
-    datagen, headers = multipart_encode({
-            "t" : "upload",
-            "file" : dfile,
-            })
-    print str(headers)
-    tahoe_url = settings.TAHOE_UPLOAD_BASE
-    request = urllib2.Request(tahoe_url, datagen, headers)
+    datagen, headers = multipart_encode((
+            ("t","upload"),
+            MultipartParam(name='file',fileobj=source_file,
+                           filename=filename)))
+    request = urllib2.Request(settings.TAHOE_UPLOAD_BASE, datagen, headers)
     cap = urllib2.urlopen(request).read()
+    source_file.close()
+
     print cap
     models.add_thumb(slug,"full",cap)
     # TODO: other sizes
