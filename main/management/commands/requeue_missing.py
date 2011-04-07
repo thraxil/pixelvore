f40c3269816c6ecc44742ecda335ea976c8ba179
Anders Pearson <anders@columbia.edu>
2011-04-07 11:35:59 -0400

    script to requeue images that were not imported properly

---
+++ main/management/commands/requeue_missing.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+import main.models
+import main.tasks
+
+class Command(BaseCommand):
+    args = ''
+    help = ''
+    
+    def handle(self, *args, **options):
+        for i in main.models.Image.objects.all():
+            st = i.get_stream_url()
+            tt = i.get_squarethumb_url()
+            ft = i.get_full_url()
+            if None in [st,tt,ft]:
+                print "redoing %d" % i.url
+                # clear it out
+                for t in image.thumb_set.all():
+                    t.delete()
+                main.tasks.ingest_image.delay(i.id,i.url)
