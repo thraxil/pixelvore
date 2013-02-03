6b46475e3455b26a305478a9765e543254ed2055
Anders Pearson <anders@columbia.edu>
2013-02-02 21:43:19 -0500

    run tests on travis

--- settings_shared.py
+++ settings_shared.py
@@ -1,5 +1,6 @@
 # Django settings for pixelvore project.
 import os.path
+import sys
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -8,12 +9,33 @@ ADMINS = ( )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'pixelvore' # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pixelvore',
+        'HOST': '',
+        'PORT': 5432,
+        'USER': '',
+        'PASSWORD': '', }
+}
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+            'HOST': '',
+            'PORT': '',
+            'USER': '',
+            'PASSWORD': '', }}
+
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=pixelvore',
+]
 
 TIME_ZONE = 'America/New_York'
 LANGUAGE_CODE = 'en-us'
@@ -57,6 +79,7 @@ INSTALLED_APPS = (
     'munin',
     'djcelery',
     'main',
+    'django_nose',
 )
 
 THUMBNAIL_SUBDIR = "thumbs"
