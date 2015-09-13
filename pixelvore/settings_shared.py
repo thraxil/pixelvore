bd3613494f06c75bb86286460ef6ddfc024801c1
Anders Pearson <anders@columbia.edu>
2015-09-13 17:18:19 +0200

    convert to thraxilsettings

--- pixelvore/settings_shared.py
+++ pixelvore/settings_shared.py
@@ -1,129 +1,19 @@
 # Django settings for pixelvore project.
 import os.path
-import sys
+from thraxilsettings.shared import common
 import djcelery
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+app = 'pixelvore'
+base = os.path.dirname(__file__)
 
-ADMINS = ()
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pixelvore',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '', }
-}
-
-if 'test' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '', }}
-
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/pixelvore/uploads/"
-MEDIA_URL = '/uploads/'
-ADMIN_MEDIA_PREFIX = '/media/'
-SECRET_KEY = ')ng#)ef_u@_^zvvu@dxm7q-lyb_^!a6%v3v^j3b(mp+)l+5%@h'
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.debug',
-    'django.core.context_processors.request',
-    'django.core.context_processors.static',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-)
-
-ROOT_URLCONF = 'pixelvore.urls'
-
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
+locals().update(common(app=app, base=base))
 djcelery.setup_loader()
 
-INSTALLED_APPS = [
-    'django.contrib.contenttypes',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS += [  # noqa
     'djcelery',
     'pixelvore.main',
-    'django_statsd',
-    'gunicorn',
-    'django_markwhat',
-    'compressor',
-    'smoketest',
 ]
-
-STATIC_URL = "/media/"
-STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "../media/")),
-)
-STATIC_ROOT = ""
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-
-
-THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[pixelvore] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "anders@columbia.edu"
-
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', 'sitemedia'),
-)
-
-BROKER_HOST = "128.59.152.25"
-BROKER_PORT = 5672
-BROKER_VHOST = "/"
 CELERYD_CONCURRENCY = 4
 
 RETICULUM_BASE = "https://d2f33fmhbh7cs9.cloudfront.net/"
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'pixelvore'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
-
-ALLOWED_HOSTS = ['localhost', '.thraxil.org']
+ALLOWED_HOSTS += ['.thraxil.org']  # noqa
