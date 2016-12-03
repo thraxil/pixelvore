05c80aa01d7fe04dc5eb9161202e0e2da965cedc
Anders Pearson <anders@columbia.edu>
2016-12-03 17:24:07 +0000

    django 1.10 and new celery setup

--- pixelvore/settings_shared.py
+++ pixelvore/settings_shared.py
@@ -1,16 +1,13 @@
 # Django settings for pixelvore project.
 import os.path
 from thraxilsettings.shared import common
-import djcelery
 
 app = 'pixelvore'
 base = os.path.dirname(__file__)
 
 locals().update(common(app=app, base=base))
-djcelery.setup_loader()
 
 INSTALLED_APPS += [  # noqa
-    'djcelery',
     'pixelvore.main',
 ]
 CELERYD_CONCURRENCY = 4
